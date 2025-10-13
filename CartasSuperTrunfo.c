8cba6609d33ea8ef36626cd0a46175890b45b22f
Kleber <kleber-pastori@hotmail.com>
2025-10-12 23:10:32 -0300

    Update CartasSuperTrunfo.c
    Codigo criado!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado[50];
+char nomeDaCarta[50];
+char nomeDaCidade[50];
+int populacao;
+float area;
+float pib;
+int pontosTuristicos;
+    
   // Área para entrada de dados
+printf("Qual estado?\n");
+  scanf(" %[^\n]", estado);  // Lê string com espaços
 
-  // Área para exibição dos dados da cidade
+printf("Qual codigo da carta?\n");
+  scanf(" %[^\n]", nomeDaCarta);
+
+printf("Qual a cidade?\n");
+   scanf(" %[^\n]", nomeDaCidade);
+
+ printf("Qual a população?\n");
+   scanf("%d", &populacao);
+
+ printf("Qual a área?\n");
+   scanf("%f", &area);
+
+ printf("Qual o PIB?\n");
+   scanf("%f", &pib);
+
+ printf("Quantos pontos turísticos?\n");
+   scanf("%d", &pontosTuristicos);
+  
+    // Área para exibição dos dados da cidade
+printf("\nCarta A\n");
+printf("- Estado: %s\n", estado);
+printf("- Código da carta: %s\n", nomeDaCarta);
+printf("- Cidade: %s\n", nomeDaCidade);
+printf("- População: %d\n", populacao);
+printf("- Área: %.2f km²\n", area);
+printf("- PIB: %.2f bilhões\n", pib);
+printf("- Pontos turísticos: %d\n", pontosTuristicos);
 
 return 0;
 } 
